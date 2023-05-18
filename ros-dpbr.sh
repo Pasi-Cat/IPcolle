2e9bdb4390f4180cc7d29bb978d9449facd62019
Pasi-Cat <122531287+Pasi-Cat@users.noreply.github.com>
2023-05-18 16:35:39 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,16 +3,16 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+wget --no-check-certificate -c -O AS4809.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
 
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+for net in $(cat AS4809.txt) ; do
+  echo "add list=AS4809 address=$net"
 done
 
-} > ../CN.rsc
+} > ../AS4809.rsc
 
 cd ..
 rm -rf ./pbr
